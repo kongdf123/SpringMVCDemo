47897f50e946b4e61ed948102747109ab0cbda6f
kongdf <kongdf@192.168.211.252>
2017-09-04 22:08:27 +0800

    Create a Spring MVC Demo

---
+++ src/main/java/com/springmvc/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value="/hello")
+public class HelloController {
+	
+	@RequestMapping(value="/mvc",method=RequestMethod.GET)
+	public String Helloworld(Model model) {
+		model.addAttribute("message", "Hello Spring MVC!");
+		
+		return "HelloWorld";
+	}
+}
